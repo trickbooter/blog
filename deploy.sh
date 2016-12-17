9ad707c63ce31334068e07811beb3164f8870a77
trickbooter <paul@trickbooter.com>
2016-12-17 18:50:45 +1100

    rebuilding site Sat 17 Dec 2016 18:50:45 AEDT

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,15 @@ echo -e "\033[0;32mCommitting changes...\033[0m"
 # Build the project.
 hugo
 
-# Add changes to git.
-git add -A
-
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+# Add changes to git.
+git add -A
 git commit -m "$msg"
-git push origin develop
+git push
 
 # Push source and build repos.
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
