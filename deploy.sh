606c7d7146a0a385bcab325ce302b7cdca36f63c
trickbooter <paul@trickbooter.com>
2016-12-17 18:49:17 +1100

    rebuilding site Sat 17 Dec 2016 18:49:17 AEDT

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mCommitting changes...\033[0m"
 
 # Build the project.
 hugo
@@ -14,6 +14,8 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Push source and build repos.
 git push origin develop
